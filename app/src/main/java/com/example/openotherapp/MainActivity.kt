f8b08f526c787a47d32619a5c622ce5cbdf1c798
seokmin <14891@naver.com>
2024-05-05 19:50:07 +0900

    FEATURE get package name and open other app -Add a feature to filter only the apps that can be opened later

--- app/src/main/java/com/example/openotherapp/MainActivity.kt
+++ app/src/main/java/com/example/openotherapp/MainActivity.kt
@@ -1,33 +1,43 @@
 package com.example.openotherapp
 
-import android.annotation.SuppressLint
+import android.R
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageInfo
+import android.os.Build
 import android.os.Bundle
 import android.provider.AlarmClock
 import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.openotherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button:Button
     private lateinit var button2:Button
     private lateinit var binding: ActivityMainBinding
+
+    private var query = "com.android.chrome"
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
+        getApp()
         button = binding.button
-        button.setOnClickListener() {
+        button.setOnClickListener {
             dispatchTakePictureIntent()
         }
         button2 = binding.button2
         button2.setOnClickListener() {
-            var intent = Intent("com.example.moti.search")
+            val intent = packageManager.getLaunchIntentForPackage(query)
             startActivity(intent)
         }
         setContentView(binding.root)
@@ -62,4 +72,47 @@ class MainActivity : AppCompatActivity() {
             // Display error state to the user.
         }
     }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getApp() {
+        val result : List<PackageInfo> = getPackageManager().getInstalledPackages(0)    //특별한 경우 제외하곤 거의 0
+        val appData = arrayListOf<String>()
+        if(result != null){
+            for(info in result){
+                val packageName = info.packageName
+                val appName = info.applicationInfo.loadLabel(packageManager)
+                val category = ApplicationInfo.getCategoryTitle(this, info.applicationInfo.category)
+                Log.d("APPINFO","$packageName")
+//                appData.add(packageName+"   ::"+appName+" <"+category+">\n")
+                appData.add(packageName)
+            }
+        }
+        setupSpinner(appData)
+    }
+
+    private fun setupSpinner(items: ArrayList<String>) {
+        val spinner= binding.spinner
+
+        // Create an adapter to specify the layout for each item and populate the spinner
+        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, items)
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+
+        // Apply the adapter to the spinner
+        spinner.adapter = adapter
+
+        // Optionally, you can set a listener to respond to item selection
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                // Handle item selection here
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                query = selectedItem
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+    }
+
 }
